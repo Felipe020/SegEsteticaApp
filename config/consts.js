bd8c3e41bfa075e7723ff82137306725c15e713f
AyresMonteiro <ayresmonteiro52@gmail.com>
2021-11-04 17:39:27 -0300

    conecta ao backend (mas nao integra)

--- config/consts.js
+++ config/consts.js
@@ -1,1 +1,1 @@
-export const API_URL = 'https://segestetica.herokuapp.com/api'
+export const API_URL = 'https://segestetica.herokuapp.com/api';
--- pages/Cadastrar/Estabelecimento/conexao.js
+++ pages/Cadastrar/Estabelecimento/conexao.js
@@ -1,10 +1,31 @@
-import {APP_URL} from '../../../config/consts'
+import { API_URL } from '../../../config/consts';
 
-export const cadastro = async () => {
-    try{
-        const resposta = await fetch (APP_URL + '/establishments', {method:'GET'})
-        console.log(resposta)
-    }catch(e){
-        console.log(e)
+/* Repare que:
+    *   Adicionei um parâmetro "dados" na função
+    *
+    *   Você *TERIA* que checar se cada um dos dados é diferente de nulo,
+    *   se eu já não tivesse feito essa análise no backend.
+    *   Todavia, o backend retorna um erro e você vai dar seus pulos pra
+    *   descobrir como caralhos fazer a mensagem de erro aparecer na tela
+    *   😎👍
+    * 
+    *   Outra coisa que você vai ter que fazer é aprender a usar o método POST
+    *   usando o fetch() e como passar os dados através dele.
+    * 
+    *   No mais, era isso. Boa sorte.
+
+    */
+export const cadastro = async (dados) => {
+    try {
+        // Devolve uma resposta em dados binários no formato JSON.
+        const resposta = await fetch(API_URL + '/establishments', { method: 'GET' });
+
+        /* Converte o JSON binário para um objeto do JavaScript.
+            *   Mas não, você não precisa dele aqui, isso é só código exemplar.
+            *   😎👍
+        */
+        // const json = await resposta.json();
+    } catch (e) {
+        console.info(e);
     }
-} 
+};
