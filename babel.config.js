50d31ac38b497687c9cfce507bc8ad933fa1565a
AyresMonteiro <ayresmonteiro52@gmail.com>
2021-11-09 13:10:25 -0300

    fix assets resolver

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,22 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      ['module-resolver', {
+        root: ['./'],
+        alias: {
+          "assets": './src/assets',
+          "components": './src/components',
+          "config": './src/config',
+          "constants": './src/constants',
+          "hooks": './src/hooks',
+          "modules": './modules',
+          "lib": './lib',
+          "pages": './src/pages',
+          "types": './src/types',
+        },
+      }],
+    ],
   };
 };
