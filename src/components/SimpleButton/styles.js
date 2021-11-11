c223f9d4cad21b5072cc3984b2a8e9720aeb2aba
AyresMonteiro <ayresmonteiro52@gmail.com>
2021-11-11 08:15:51 -0300

    size up components

--- src/components/SimpleButton/styles.js
+++ src/components/SimpleButton/styles.js
@@ -3,6 +3,7 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   btnStyle: {
     width: '80%',
+    minHeight: 55,
     paddingHorizontal: 25,
     paddingVertical: 15,
     backgroundColor: '#202020',
--- src/components/SimpleTextInput/styles.js
+++ src/components/SimpleTextInput/styles.js
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   textInput: {
     width: '100%',
-    height: 40,
+    height: 55,
     backgroundColor: 'white',
     borderRadius: 20,
     paddingLeft: 10
