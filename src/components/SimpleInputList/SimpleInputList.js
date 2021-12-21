43b6f4c41cf31536edf7bc4ec82bab8725e0f88f
Felipe020 <felipesachetto2004@gmail.com>
2021-12-21 18:11:33 -0300

    corrigindo problema de key no componente SimpleInputList

--- src/components/SimpleInputList/SimpleInputList.js
+++ src/components/SimpleInputList/SimpleInputList.js
@@ -1,52 +1,58 @@
-import React from 'react';
-import { SimpleLabel } from '../SimpleLabel';
-import { SimplePickerInput } from '../SimplePickerInput';
-import { SimpleTextInput } from '../SimpleTextInput';
-import { styles } from './styles';
+import React, { Fragment } from "react";
+import { SimpleLabel } from "../SimpleLabel";
+import { SimplePickerInput } from "../SimplePickerInput";
+import { SimpleTextInput } from "../SimpleTextInput";
+import { styles } from "./styles";
 
 export const SimpleInputList = ({
   formState = {},
-  setPropOfState = (propName, propValue) => { },
-  fields = [{
-    name: "examplefield",
-    label: "Example",
-    type: "picker",
-    options: [{ label: "Example", value: "example" }],
-  }]
+  setPropOfState = (propName, propValue) => {},
+  fields = [
+    {
+      name: "examplefield",
+      label: "Example",
+      type: "picker",
+      options: [{ label: "Example", value: "example" }],
+    },
+  ],
 }) => {
   const getInputByfield = (field, idx) => {
-    const key = `${idx}:${field.name}`;
-
     switch (field.type) {
-      case 'picker':
-        return <SimplePickerInput
-          key={key}
-          value={formState[field.name] || 0}
-          label={field.label}
-          changeFunction={(val) => setPropOfState(field.name, val)}
-          style={styles.formItemStyle}
-          options={field.options}
-        />;
+      case "picker":
+        return (
+          <SimplePickerInput
+            value={formState[field.name] || 0}
+            label={field.label}
+            changeFunction={(val) => setPropOfState(field.name, val)}
+            style={styles.formItemStyle}
+            options={field.options}
+          />
+        );
       default:
-        return <SimpleTextInput
-          style={styles.formItemStyle}
-          key={key}
-          changeFunction={(val) => setPropOfState(field.name, val)}
-          label={field.label}
-          isSecure={field.isSecure}
-          data={formState[field.name] || ""}
-        />;
-    };
+        return (
+          <SimpleTextInput
+            style={styles.formItemStyle}
+            changeFunction={(val) => setPropOfState(field.name, val)}
+            label={field.label}
+            isSecure={field.isSecure}
+            data={formState[field.name] || ""}
+          />
+        );
+    }
   };
 
   return (
     <>
       {fields.map((field, idx) => {
+        const key = `${idx}:${field.name}`;
         return (
-          <>
-            <SimpleLabel text={field.label + ":"} outsideStyle={styles.formItemLabelStyle} />
+          <Fragment key={key}>
+            <SimpleLabel
+              text={field.label + ":"}
+              outsideStyle={styles.formItemLabelStyle}
+            />
             {getInputByfield(field, idx)}
-          </>
+          </Fragment>
         );
       })}
     </>
