4789c98eb30a170d01770b8e172d000c6bee332d
AyresMonteiro <ayresmonteiro52@gmail.com>
2022-01-06 17:46:09 -0300

    adds app color to status bar

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 
 import { SegEsteticaGlobalContext } from "@contexts";
@@ -7,6 +8,8 @@ import { MainStack } from '@stacks';
 export default function App() {
   return (
     <SegEsteticaGlobalContext>
+      <StatusBar barStyle="light-content" backgroundColor="#2E2E2E" />
+
       <NavigationContainer >
         <MainStack />
       </NavigationContainer>
