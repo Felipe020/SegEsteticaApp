afedef28eb1a9977f36323d412d89bb5c8f3fb2a
AyresMonteiro <ayresmonteiro52@gmail.com>
2022-01-08 13:16:34 -0300

    updates components/SimpleButton/index.d.ts

--- src/components/SimpleButton/index.d.ts
+++ src/components/SimpleButton/index.d.ts
@@ -1,7 +1,7 @@
 import React from "react"
 import { StyleProp } from "react-native"
 
-export type SimpleButton = React.FC<{
+export const SimpleButton: React.FC<{
 	onPress: Function,
 	outsideStyle: StyleProp<Object>,
 	text: String,
