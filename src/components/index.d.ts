80f83dc15221397e10e75063c06d6007f83dd58f
AyresMonteiro <ayresmonteiro52@gmail.com>
2022-01-08 12:08:00 -0300

    creates components typescript index

---
+++ src/components/index.d.ts
@@ -0,0 +1,6 @@
+export * from './CustomTabBar/index';
+export * from './EstablishmentCard/index';
+export * from './SimpleButton/index';
+export * from './SimpleInputList/index';
+export * from './SimplePickerInput/index';
+export * from './SimpleTextInput/index';
